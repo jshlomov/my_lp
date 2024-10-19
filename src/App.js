27113182bbcec945b67c21afb39ae3b7ff670da5
jshlomov <jonatanshv@gmail.com>
2024-10-20 00:30:46 +0300

    asdmk

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
 import Navbar from './Navbar';
 import Section1 from './Section1';
 import Section2 from './Section2';
 import Section3 from './Section3';
 import Footer from './Footer';
 
+// import { initializeApp } from 'firebase/app';
+
+// // Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB4okOOcB6r1bnFhrWJc6SKLtOOhpRTHPg",
+//   authDomain: "real-eastate-lp.firebaseapp.com",
+//   projectId: "real-eastate-lp",
+//   storageBucket: "real-eastate-lp.appspot.com",
+//   messagingSenderId: "1089559572758",
+//   appId: "1:1089559572758:web:ef536b368ac0e11a786037",
+// };
+
+// // Initialize Firebase
+// initializeApp(firebaseConfig);
+
 const App = () => {
-    return (
-        <div>
-            <Navbar />
-            <Section1 />
-            <Section2 />
-            <Section3 />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/Section1.js
+++ src/Section1.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import video from './bbb.mp4'
 
-<style>
-    
-</style>
+
 const Section1 = () => {
     return (
         <section id="Section1" style={{ position: 'relative', height: '100vh', overflow: 'hidden', backgroundColor: '#232323' }}>
-            <video autoPlay loop muted style={{ width: '100%', height: '100%', objectFit: 'cover', opacity: 0.8 }}>
+            <video autoPlay loop muted playsInline style={{ width: '100%', height: '100%', objectFit: 'cover', opacity: 0.8 }}>
                 <source src={video} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: '#fff', textAlign: 'center' }}>
                 <h1 style={{fontSize:'60px'}}>Skyline Estates</h1>
-                <h2 style={{fontSize:'30px', marginInline:'20px'}}>Skyline Estates: Expert real estate guidance to find your dream property. Experience seamless buying, selling, and investing with personalized care and trust.</h2>
+                <h2 style={{fontSize:'30px', marginInline:'20px'}}>Skyline Eystates: Expert real estate guidance to find your dream property. Experience seamless buying, selling, and investing with personalized care and trust.</h2>
             </div>
         </section>
     );
